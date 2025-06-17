da1d790a8130438d0048c568d7624b4092455f63
furqanahmadbasra <furqanahmadbasra@gmail.com>
2025-06-17 12:08:52 +0500

    add to render

--- eventify_back_end/index.js
+++ eventify_back_end/index.js
@@ -17,7 +17,8 @@ app.use(cors())
 
 // app.use(cors(corsOptions)); // Apply CORS configuration
 
-const port = 5000
+const port = process.env.PORT || 5000;
+
 // app.use(express.json())// to use req.body we need to use the express.json middleware
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
